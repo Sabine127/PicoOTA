d0ad1177529c286b1423109d43d2dcf9c6b3344f
Sabine127 <92714519+Sabine127@users.noreply.github.com>
2024-12-12 16:50:52 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from ota import OTAUpdater
 from WIFI_CONFIG import SSID, PASSWORD
 
-firmware_url = "https://drive.google.com/drive/folders/1sw3sqLbzB5_f0jwnQ1XUyzXsikQrHChz"
+firmware_url = "https://github.com/Sabine127/PicoOTA.git"
 
 ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "test.py")
 ota_updater.download_and_install_update_if_available()
